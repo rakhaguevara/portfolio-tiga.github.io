f83a33832996fd8a5b7d4a1a8ba6c12c6cf5f910
RakhaGuevara <rakha.guevara2505@gmail.com>
2024-11-25 15:25:24 +0700

    "perubahan drastis 3.0"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,55 @@
 module.exports = {
   content: ["index.html"],
 
-  darkMode: "class",
+  darkMode: "class", // Mengaktifkan mode gelap dengan class "dark"
 
   theme: {
     container: {
-      center: true,
-      padding: "16px",
+      center: true, // Semua container akan di tengah secara default
+      padding: "16px", // Padding default untuk container
     },
 
     extend: {
       colors: {
-        primary: "#14b8a6",
-        dark: "#0f172a",
-        secondary: "#64748b",
+        primary: "#14b8a6", // Warna utama
+        dark: "#0f172a", // Warna untuk mode gelap
+        secondary: "#64748b", // Warna sekunder
       },
       screens: {
-        "2xl": "1320px",
+        "2xl": "1320px", // Ukuran layar tambahan
+      },
+      animation: {
+        typing: "typing 2s steps(30, end), blink-caret 0.75s step-end infinite", // Animasi mengetik
+        fadeIn: "fadeIn 0.5s ease-in-out", // Animasi fade in
+        fadeOut: "fadeOut 0.5s ease-in-out", // Animasi fade out
+        fadeInUp: "fadeInUp 1s ease-out forwards", // Animasi gambar muncul dari bawah
+      },
+      keyframes: {
+        typing: {
+          "0%": { width: "0%" },
+          "100%": { width: "100%" },
+        },
+        "blink-caret": {
+          "50%": { borderColor: "transparent" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+        fadeInUp: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
       },
     },
   },
